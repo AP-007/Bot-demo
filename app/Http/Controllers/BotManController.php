bd465d541c2fa094bd708b9dd7dcfc0af259fbe5
AP-007 <poudel0070@gmail.com>
2022-08-18 22:22:40 +0545

    update

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -8,40 +8,42 @@ use BotMan\BotMan\Messages\Incoming\Answer;
 
 class BotManController extends Controller
 {
-    /**
-     * Place your BotMan logic here.
-     */
-    public function handle()
-    {
+  /**
+   * Place your BotMan logic here.
+   */
+  public function handle()
+  {
 
-         $botman = app('botman');
-         $botman->hears('{message}', function($botman, $message) {
-         if ($message == 'Hello')
-          {
-            $this->askName($botman);
-          }
-          else
-          {
-            $botman->reply("Wait for few moments for response. ");
-          }
-       });
-
-       $botman->listen();
-
-    }
-
-      /**
-       * Place your BotMan logic here.
-      */
-
-      public function askName($botman)
-      {
-
-          $botman->ask('Hi, What is your name ?', function(Answer $answer) {
-          $name = $answer->getText();
-
-          $this->say('Nice to meet you '.$name.". Please wait for few moments until we are back.");
-        });
-
+    $botman = app('botman');
+    $botman->hears('{message}', function ($botman, $message) {
+      if ($message == 'Hello' || 'hello' || 'Hi' || 'hi') {
+        $this->askName($botman);
+      } else {
+        $botman->reply("Wait for few moments for response. ");
       }
+    });
+
+    $botman->listen();
+  }
+
+  /**
+   * Place your BotMan logic here.
+   */
+
+  public function askName($botman)
+  {
+    $botman->ask('Hi, What is your name ?', function (Answer $answer) {
+      $name = $answer->getText();
+
+      $this->say('Nice to meet you '.strtoupper($name));
+    });
+  }
+  // public function askEmail($botman)
+  // {
+  //   $botman->ask('Can I know your email ?', function (Answer $answer) {
+  //     $email = $answer->getText();
+
+  //     $this->say('Thankyou. I will mail you at'.strtoupper($email));
+  //   });
+  // }
 }
